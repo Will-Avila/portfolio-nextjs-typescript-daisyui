8142f81a6eda3f7abf9146e035de0e1f0d28a440
Will-Avila <willaviladev@gmail.com>
2025-01-02 12:37:38 -0300

    Atualizacoes_02-01-2025

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,33 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Montserrat } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+import { Montserrat } from "next/font/google";
+import Script from "next/script";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-})
+});
 
 export const metadata: Metadata = {
   title: "Will Avila | Desenvolvedor Web | Portfólio | Fortaleza-Ce",
-  description: "Portfólio Will Avila - Desenvolvedor Web com habilidades em tecnologias modernas, criando soluções digitais inovadoras e responsivas"
+  description:
+    "Portfólio Will Avila - Desenvolvedor Web com habilidades em tecnologias modernas, criando soluções digitais inovadoras e responsivas",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-BR" data-theme="dark" className="scroll-smooth">
       <body className={montserrat.className}>
+        {/* Script do Cloudflare Turnstile */}
+        <Script
+          src="https://challenges.cloudflare.com/turnstile/v0/api.js"
+          strategy="afterInteractive"
+        />
         {children}
       </body>
     </html>
